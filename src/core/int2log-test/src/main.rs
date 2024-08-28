2fe2ea16aecd730154cb2a50910f3001495379d7
Intellectus-Shinhaeseul <hs.shin@int2.us>
2024-08-28 16:34:51 +0900

    FEAT: 라이브러리들 테스트용

---
+++ src/core/int2log-test/src/main.rs
@@ -0,0 +1,37 @@
+use int2log_core::*;
+use int2log_model::*;
+use int2log_zenoh::*;
+
+// #[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+async fn main() {
+	/* Log version 1) 
+	 Log + Capnp + Zenoh => 에러로 인해 동작하지 않는 로그 (테스트는 작동 O) */
+	// let zenoh_config = ZenohConfiguration{
+	// 	config: Default::default(),
+	// 	pub_key: Some(String::from("topic/test")),
+	// 	sub_key: None,
+	// };
+	// let middleware = ZenohMiddlewareBuilder::default().config(zenoh_config).await.unwrap().build().await.unwrap();
+	// let mut log: Log<Vec<u8>, CapnpSerializer, ZenohMiddleware> = Log::log().serializer(CapnpSerializer).middleware(middleware);
+	
+	/* Log version 2) 
+	 Log + DefaultSerializer(Data to String or Vec<u8>, not Capnp) + DefaultMiddleware(Send, Receive - X) */
+	// let mut log: Log<String, DefaultSerializer, DefaultMiddleware> = Log::default();
+
+	// Log version 3) Log + Capnp + DefaultMiddleware(Send, Receive - X)
+	let mut log: Log<Vec<u8>, CapnpSerializer, DefaultMiddleware> = Log::log().serializer(CapnpSerializer).middleware(DefaultMiddleware);
+	
+	log.debug(String::from(format!("Default Setting: {:?}", log))).await;
+	log.set_publish_level(LogLevel::Warn);
+	log.debug(String::from(format!("Change Pub log level: {:?}", log))).await;
+	log.set_print_level(LogLevel::Debug);
+	log.debug(String::from(format!("Change Print log level: {:?}", log))).await;
+	
+	log.trace(String::from("This is Trace!")).await;
+	log.debug(String::from("This is Debug!")).await;
+	log.info(String::from("This is Info!")).await;
+	log.warn(String::from("This is Warn!")).await;
+	log.error(String::from("This is Error!")).await;
+	log.debug(String::from(format!("log: {:?}", log))).await;
+}
