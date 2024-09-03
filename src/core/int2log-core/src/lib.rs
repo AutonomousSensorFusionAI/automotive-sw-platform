c778231e85e81cb53ad1fcad8b4e32e449775f63
Intellectus-Shinhaeseul <hs.shin@int2.us>
2024-09-03 18:02:42 +0900

    FEAT: common과 공통된 내용 제거

--- src/core/int2log-core/src/lib.rs
+++ src/core/int2log-core/src/lib.rs
@@ -3,47 +3,10 @@ use derivative::Derivative;
 use std::marker::PhantomData;
 use std::future::Future;
 use tokio::runtime::Runtime;
+use int2log_model::*;
+use int2log_zenoh::*;
+use int2log_common::*;
 
-pub trait Serialization<T> {
-	fn serialize_msg(&self, t: &LogMessage) -> T;
-	fn deserialize_msg(&self, t: &T) -> LogMessage;
-}
-
-pub trait Communication<T> {
-	fn sender(&self, t: T) -> impl Future<Output = ()>; // async fn
-}
-
-#[repr(i32)]
-#[derive(Debug, Default, PartialEq, PartialOrd, Copy, Clone)]
-pub enum LogLevel {
-	Trace = 0, 
-	#[default]
-	Debug = 1, // This is Default Value of Log Level
-	Info = 2, 
-	Warn = 3,
-	Error = 4,
-}
-
-impl LogLevel {
-	pub fn from_str(level_str: &str) -> Option<Self>{
-		match level_str {
-			"Trace" => Some(LogLevel::Trace),
-			"Debug" => Some(LogLevel::Debug),
-			"Info" => Some(LogLevel::Info),
-			"Warn" => Some(LogLevel::Warn),
-			"Error" => Some(LogLevel::Error),
-			_ => None,
-		}
-	}
-}
-
-#[repr(C)]
-#[derive(Debug, Default)]
-pub struct LogMessage {
-	pub log_level: LogLevel,
-	pub msg: String,
-	pub timestamp: String,
-}
 
 #[repr(C)]
 #[derive(Derivative)]
