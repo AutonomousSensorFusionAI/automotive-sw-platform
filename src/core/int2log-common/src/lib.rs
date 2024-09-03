25a19a519d0ea07bc049a9d4cda1c77bb6daa6c8
Intellectus-Shinhaeseul <hs.shin@int2.us>
2024-09-03 17:59:44 +0900

    CHORE: 의존성 순환 해결을 위한 내용 추가 (core->common)

--- src/core/int2log-common/src/lib.rs
+++ src/core/int2log-common/src/lib.rs
@@ -1,5 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::future::Future;
+
+#[repr(i32)]
+#[derive(Debug, Default, PartialEq, PartialOrd, Copy, Clone)]
+pub enum LogLevel {
+	Trace = 0, 
+	#[default]
+	Debug = 1, // This is Default Value of Log Level
+	Info = 2, 
+	Warn = 3,
+	Error = 4,
+}
+
+impl LogLevel {
+	pub fn from_str(level_str: &str) -> Option<Self>{
+		match level_str {
+			"Trace" => Some(LogLevel::Trace),
+			"Debug" => Some(LogLevel::Debug),
+			"Info" => Some(LogLevel::Info),
+			"Warn" => Some(LogLevel::Warn),
+			"Error" => Some(LogLevel::Error),
+			_ => None,
+		}
+	}
+}
+
+#[repr(C)]
+#[derive(Debug, Default)]
+pub struct LogMessage {
+	pub log_level: LogLevel,
+	pub msg: String,
+	pub timestamp: String,
+}
+
+pub trait Serialization<T> {
+	fn serialize_msg(&self, t: &LogMessage) -> T;
+	fn deserialize_msg(&self, t: &T) -> LogMessage;
+}
+
+pub trait Communication<T> {
+	fn sender(&self, t: T) -> impl Future<Output = ()>; // async fn
 }
 
 #[cfg(test)]
@@ -8,7 +47,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+
     }
 }
