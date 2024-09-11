36d3fbe17fe5e2120ecc22706f6b6c95668b819a
Intellectus-Shinhaeseul <hs.shin@int2.us>
2024-09-11 14:47:51 +0900

    CHORE: common -> model 경로 수정

--- src/core/int2log-test/src/main.rs
+++ src/core/int2log-test/src/main.rs
@@ -1,6 +1,7 @@
 use int2log_core::*;
-use int2log_common::*;
 use int2log_model::*;
+use int2log_model::log_level::*;
+use int2log_model::log_message::*;
 use int2log_zenoh::*;
 
 // #[tokio::main]
@@ -8,7 +9,6 @@ use int2log_zenoh::*;
 async fn main() {
 	/* Log version 1) 
 	 Log + Capnp + Zenoh  */
-	// let mut log = Log::default().await;
 	let mut log = Log::default().await;
 	/* Log version 2) 
 	 Log + DefaultSerializer(Data to String or Vec<u8>, not Capnp) + DefaultMiddleware(Send, Receive - X) */
@@ -17,7 +17,15 @@ async fn main() {
 	// Log version 3) Log + Capnp + DefaultMiddleware(Send, Receive - X)
 	// let mut log: Log<Vec<u8>, CapnpSerializer, DefaultMiddleware> = Log::log().serializer(CapnpSerializer).middleware(DefaultMiddleware);
 	
+	// let middleware = ZenohMiddleware::default().await;
+	// let mut log: Log<String, DefaultSerializer, ZenohMiddleware> = Log::log().serializer(DefaultSerializer).middleware(middleware);
+
 	log.debug(String::from(format!("Default Setting: {:?}", log))).await;
+	log.trace(String::from("This is Trace!")).await;
+	log.debug(String::from("This is Debug!")).await;
+	log.info(String::from("This is Info!")).await;
+	log.warn(String::from("This is Warn!")).await;
+	log.error(String::from("This is Error!")).await;
 	log.set_publish_level(LogLevel::Warn);
 	log.debug(String::from(format!("Change Pub log level: {:?}", log))).await;
 	log.set_print_level(LogLevel::Debug);
@@ -28,5 +36,5 @@ async fn main() {
 	log.info(String::from("This is Info!")).await;
 	log.warn(String::from("This is Warn!")).await;
 	log.error(String::from("This is Error!")).await;
-	log.debug(String::from(format!("log: {:?}", log))).await;
+	log.debug(String::from(format!("log: {:?}", log.log_message))).await;
 }
