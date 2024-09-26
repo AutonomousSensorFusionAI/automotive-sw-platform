5d07f876ea42daaf6f7d4354cb8b2e749c1cd65d
Intellectus-Shinhaeseul <hs.shin@int2.us>
2024-09-26 15:35:47 +0900

    CHORE: library 수정에 따른 test 코드 수정 및 core의 테스트 코드와 동일한 내용 삭제

--- src/core/int2log-test/src/main.rs
+++ src/core/int2log-test/src/main.rs
@@ -16,34 +16,8 @@ fn main() {
 #[test]
 fn log_works() {
 	let mut log = Log::default();
-	let my_console = log.logger.default_console();
+	let my_console = log.logger.borrow_mut().default_console();
 	my_console.borrow_mut().set_log_level("error");
 	println!("{:?}", log);
 	log.error("Hi, Error!".to_string());
-}
-
-#[test]
-fn make_logger_ex() {
-	use std::{
-		rc::Rc,
-		cell::RefCell,
-	};
-	use int2log_model::log_level::*;
-	use int2log_model::log_message::*;
-
-
-	let mut logger = Logger::new();
-		let console_logger_a = Rc::new(RefCell::new(ConsoleLogger::default()));
-		let console_logger_b = Rc::new(RefCell::new(ConsoleLogger::default()));
-		let file_logger = Rc::new(RefCell::new(FileLogger::default()));
-		console_logger_b.borrow_mut().set_log_level("error");
-		let middleware = Rc::new(RefCell::new(MiddlewareLogger::default()));
-		logger.attach(console_logger_a.clone());
-		logger.attach(console_logger_b.clone());
-		logger.attach(file_logger.clone());
-		logger.attach(middleware.clone());
-		// let mut log = Log::default();
-		let mut log = Log::new("debug", logger);
-		println!("{:?}", log);
-		log.error("Hi, Error!".to_string());
 }
