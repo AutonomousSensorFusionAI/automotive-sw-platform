cba075d32370788dc71413e1ee69f8d448de6d95
Intellectus-Shinhaeseul <hs.shin@int2.us>
2024-10-01 14:59:00 +0900

    TEST: 샘플 프로젝트 추가

--- src/tools/int2log-subscriber/src/main.rs
+++ src/tools/int2log-subscriber/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use int2log_zenoh::*;
+
+#[tokio::main]
+async fn main() {
+    let zenoh_config = ZenohConfiguration {
+        config: Default::default(),
+        pub_key: None,
+        sub_key: Some(String::from("log")),
+    };
+    let middleware = ZenohMiddlewareBuilder::default()
+        .config(zenoh_config)
+        .build()
+        .await
+        .unwrap();
+
+    loop {
+        middleware.receiver_capnp().await
+    }
 }
