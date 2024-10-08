475a65cea588c28c0d22db25613fe7703c061b8f
Intellectus-Shinhaeseul <hs.shin@int2.us>
2024-10-08 16:24:07 +0900

    FEAT: int2log 활용해서 pub/sub 5분동안 진행하는 샘플로 변경

--- src/tools/int2log-publisher/src/main.rs
+++ src/tools/int2log-publisher/src/main.rs
@@ -1,14 +1,113 @@
 use int2log_core::*;
-use std::{cell::RefCell, rc::Rc, thread, time};
+use int2log_model::serializer::SerializerFactory;
+use int2log_zenoh::*;
+use std::{
+    cell::RefCell,
+    rc::Rc,
+    thread,
+    time::{Duration, Instant},
+};
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    // Zenoh Setting
+    let zenoh_config = ZenohConfiguration {
+        config: Default::default(),
+        pub_key: Some(String::from("log_pub")),
+        sub_key: Some(String::from("log_sub")),
+    };
+    let middleware = Rc::new(
+        ZenohMiddlewareBuilder::default()
+            .config(zenoh_config)
+            .build()
+            .await
+            .unwrap(),
+    );
+
+    let capnp = SerializerFactory::new().capnp_serializer();
+    let mut mid_logger = Rc::new(RefCell::new(MiddlewareLogger::new(
+        capnp,
+        middleware.clone(),
+    )));
+
+    // Logger Setting
+    let mut logger = Rc::new(RefCell::new(Logger::new()));
+    logger.borrow_mut().attach(mid_logger);
+
+    let file = Rc::new(RefCell::new(FileLogger::default()));
+    logger.borrow_mut().attach(file.clone());
+    file.borrow_mut().set_file_path("log_publisher.txt");
+    file.borrow_mut().set_log_level("trace");
+    file.borrow_mut().set_active_false();
+
+    // Log Setting
+    let mut log = Log::new("debug", logger);
+
+    let hz_duration = get_duration(500);
+
+    let mut start_flag = false;
+    let health_check = Duration::from_secs(10);
+    let mut sub_flag = false;
+
+    // Subscriber 켜질 때까지 waiting (Pingpong으로 확인)
+    while !start_flag {
+        match tokio::time::timeout(health_check, middleware.receiver_capnp()).await {
+            Ok(Some((_, value))) => match value.data.as_str() {
+                "-1" if !sub_flag => {
+                    log.debug("-2".to_string());
+                    sub_flag = true;
+                }
+                "0" if sub_flag => {
+                    log.debug("0".to_string());
+                    start_flag = true;
+                }
+                _ => {}
+            },
+            Ok(None) => {}
+            Err(_) => {
+                sub_flag = false;
+                println!(
+                    "{} seconds passed without Subscriber starting. Retrying...",
+                    health_check.as_secs()
+                );
+            }
+        }
+    }
+
+    println!(
+        "Starting... Setting hz: {:?}",
+        1000 / hz_duration.as_millis()
+    );
+    thread::sleep(health_check);
+
+    let publish_duration = Duration::from_secs(5 * 60);
+    let mut count = 0;
+    let start_time = Instant::now();
+
+    while start_time.elapsed() < publish_duration {
+        count += 1;
+        log.debug(count.to_string());
+        thread::sleep(hz_duration);
+    }
+
+    log.debug("-1".to_string());
+    println!("Published {} messages", count);
+}
+
+fn get_duration(hz: u64) -> Duration {
+    let millis: u64 = 1000 / hz;
+    Duration::from_millis(millis)
+}
+
+#[test]
+fn make_log() {
     let mut log = Log::default();
     /*
     세부 로거 수정은 다음과 같이.
     let default_console = log.logger.borrow_mut().default_console();
     default_console.borrow_mut().set_log_level("error");
     */
-    let one_second = time::Duration::from_secs(1);
+    let one_second = Duration::from_secs(1);
     println!("{:?}", log);
 
     loop {
@@ -33,7 +132,7 @@ fn custom_logger() {
     /*
     custom_logger.borrow_mut().detach(console_logger);
     */
-    let one_second = time::Duration::from_secs(1);
+    let one_second = Duration::from_secs(1);
     println!("{:?}", log);
 
     loop {
--- src/tools/int2log-subscriber/src/main.rs
+++ src/tools/int2log-subscriber/src/main.rs
@@ -1,8 +1,103 @@
+use futures::FutureExt;
+use int2log_core::*;
+use int2log_model::serializer::SerializerFactory;
 use int2log_zenoh::*;
-use futures::FutureExt; // map을 사용하기 위해 필요
+use int2log_zenoh::*;
+use std::{cell::RefCell, rc::Rc, time::Duration};
 
 #[tokio::main]
 async fn main() {
+    // Zenoh Setting
+    let zenoh_config = ZenohConfiguration {
+        config: Default::default(),
+        pub_key: Some(String::from("log_sub")),
+        sub_key: Some(String::from("log_pub")),
+    };
+    let middleware = Rc::new(
+        ZenohMiddlewareBuilder::default()
+            .config(zenoh_config)
+            .build()
+            .await
+            .unwrap(),
+    );
+
+    let capnp = SerializerFactory::new().capnp_serializer();
+    let mut mid_logger = Rc::new(RefCell::new(MiddlewareLogger::new(
+        capnp,
+        middleware.clone(),
+    )));
+
+    // Logger Setting
+    let mut logger = Rc::new(RefCell::new(Logger::new()));
+    logger.borrow_mut().attach(mid_logger);
+
+    let file = Rc::new(RefCell::new(FileLogger::default()));
+    logger.borrow_mut().attach(file.clone());
+    file.borrow_mut().set_file_path("log_subscriber.txt");
+    file.borrow_mut().set_log_level("trace");
+    file.borrow_mut().set_active_false();
+
+    // Log Setting
+    let mut log = Log::new("debug", logger);
+
+    let mut start_flag = false;
+    let health_check = Duration::from_secs(10);
+    let mut pub_flag = false;
+    log.debug("-1".to_string());
+
+    // Publisher 켜질 때까지 waiting (Pingpong으로 확인)
+    while !start_flag {
+        match tokio::time::timeout(health_check, middleware.receiver_capnp()).await {
+            Ok(Some((_, value))) => match value.data.as_str() {
+                "-2" if !pub_flag => {
+                    log.debug("0".to_string());
+                    pub_flag = true;
+                }
+                "0" if pub_flag => {
+                    start_flag = true;
+                }
+                _ => {}
+            },
+            Ok(None) => {}
+            Err(_) => {
+                pub_flag = false;
+                println!(
+                    "{} seconds passed without Subscriber starting. Retrying...",
+                    health_check.as_secs()
+                );
+                log.debug("-1".to_string());
+            }
+        }
+    }
+
+    println!("Starting...");
+    let mut missing: Vec<i32> = Vec::new();
+    let mut count = 0;
+
+    while let Some((_, value)) = middleware.receiver_capnp().await {
+        count += 1;
+        let pub_count: i32 = value.data.parse().unwrap();
+        log.trace(format!("pub: {} sub: {}", pub_count, count));
+
+        match pub_count {
+            -1 => {
+                count -= 1;
+                println!("sub_count: {}, missing: {:?}", count, missing);
+                break;
+            }
+            _ if pub_count != count => {
+                for i in count..pub_count {
+                    missing.push(i);
+                }
+                count = pub_count;
+            }
+            _ => {}
+        }
+    }
+}
+
+#[tokio::test]
+async fn make_subscriber() {
     let zenoh_config = ZenohConfiguration {
         config: Default::default(),
         pub_key: None,
